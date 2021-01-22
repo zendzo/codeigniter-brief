f2414ee6f7d2563ba1fdda9b77c3177212eeb1d8
M Zaenal <zendzzo@gmail.com>
2021-01-22 09:58:16 +0700

    Initial Commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+  public function login()
+  {
+    $this->load->view('auth/login');
+  }
+}
